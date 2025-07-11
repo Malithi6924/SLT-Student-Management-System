a033de440a93a9b0942f04818d1aa3f0838a2f7f
Malithi6924 <malithimadhubashinie@gmail.com>
2025-07-11 11:51:28 +0530

    Navbar added to pages

--- frontend/src/Pages/AddStudent.js
+++ frontend/src/Pages/AddStudent.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function AddStudent() {
   return (
     <div>
+        <Navbar/>
       <h1>Add students</h1>
     </div>
   )
